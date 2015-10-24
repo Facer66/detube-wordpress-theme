0eccd0d2b78e1ebea2eb18112cfcb897107c000b
Matias Cañete <matiascaniete@gmail.com>
2015-10-24 02:38:02 +0200

    listado de categorias de ancho completo

--- wp-content/themes/detube/category.php
+++ wp-content/themes/detube/category.php
@@ -13,23 +13,23 @@ get_header(); ?>
 
 <?php get_template_part('cat-featured'); ?>
 
-<div id="main"><div class="wrap cf">
-	
+<div id="main" class="full-width"><div class="wrap cf">
+
 	<div id="content" role="main">
-	<?php 
+	<?php
 		get_template_part('loop-header');
-	
+
 		if (have_posts()) :
 			get_template_part('loop-actions');
 			get_template_part('loop-content');
 			get_template_part('loop-nav');
 		else :
 			get_template_part('loop-error');
-		endif; 
+		endif;
 	?>
 	</div><!-- end #content -->
 
-	<?php get_sidebar(); ?>
+	<?php //get_sidebar(); ?>
 
 </div></div><!-- end #main -->
 
